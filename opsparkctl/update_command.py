65e6808fcb8fa911ceecc0b6f632ce1e2c0b6737
Piotr Mrowczynski <piotr.mrowczynski@cern.ch>
2018-06-01 14:50:17 +0200

    Support update of the spark operator

---
+++ opsparkctl/update_command.py
@@ -0,0 +1,18 @@
+from . import base_command
+
+
+class KubeSparkUpdateCommand(base_command.BaseCommand):
+    def run(self, args, additional):
+        from . import kube_config
+        from . import spark_kube_client
+
+        kube_config_client = kube_config.KubeConfigClient()
+
+        c = kube_config_client.load_local_config()
+        spark_client = spark_kube_client.SparkK8SApi(c)
+
+        spark_client.update_spark_operator_base()
+        return True
+
+
+
